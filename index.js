0587180934baf9b7945f6d5bcfbadc570de5eb71
Nicole Chinn <nicole@Nicole-Chinn.local>
2016-12-16 15:45:07 +1300

    experiment with express

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+	cons = require('consolidate'),
+	app = express()
+
+app.engine('html', cons.swig)
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
+
+app.get('/', function(req, res) {
+	res.render('text', {
+		title: 'Hello World!'
+	})
+})
+
+app.use('/multiple-checkboxes', function(req, res) {
+	res.send('/checkbox');
+})
+
+app.listen(3000, function() {
+	console.log('app is listening');
+})
