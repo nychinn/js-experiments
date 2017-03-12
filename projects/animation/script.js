3cde5695ce06be617d157c3074090f847cdbea69
Nicole C <nychinn@gmail.com>
2017-03-13 10:49:32 +1300

    animation sandbox

---
+++ projects/animation/script.js
@@ -0,0 +1,22 @@
+
+var square = $('.square');
+var list = $('ul li');
+var text = $('.text-wrap');
+
+
+
+// square.velocity({opacity: 0.5});
+// square.velocity('callout.shake');
+
+// square
+//     .velocity("callout.shake", 2000)
+// 	.velocity("transition.slideDownOut");
+
+// // list.velocity("transition.slideLeftIn", { stagger: 250 });
+
+// setInterval(function() {
+//   $("ul li")
+//     .velocity("transition.slideLeftIn", { stagger: 250 })
+//     .delay(750)
+//     .velocity({ opacity: 0 }, 750)
+// }, 2000);
