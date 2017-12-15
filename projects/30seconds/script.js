a34fbc1cabb9c36301c62a83b2a4840637f06a5e
Nicole C <nychinn@gmail.com>
2017-12-15 17:52:39 +1300

    adjust shadow mixin, work through 30 seconds

--- projects/30seconds/script.js
+++ projects/30seconds/script.js
@@ -1,7 +1,52 @@
 
 
 
+// -- Array Concatenation
 const arrayConcat = (arr, ...args) => arr.concat(...args);
 
-console.log( arrayConcat([1], 2, [3], [[4]]) );
-console.log( arrayConcat( ['fruit'], 'apple', 'banana', ['moon', 'mars', 'jupiter'], [['gold', 'silver']] ) );
+var arrayConcatOriginal = "['fruit'], 'apple', 'banana', ['moon', 'mars', 'jupiter'], [['gold', 'silver']]";
+var arrayConcatOutput = arrayConcat( ['fruit'], 'apple', 'banana', ['moon', 'mars', 'jupiter'], [['gold', 'silver']] );
+
+$('.arrayConcatOriginal').text(arrayConcatOriginal);
+$('.arrayConcatOutput').text( JSON.stringify(arrayConcatOutput) );
+
+
+
+
+// -- Array Difference
+const difference = (a, b) => {
+	const s = new Set(b);
+	return a.filter(x => !s.has(x));
+};
+
+var arrayDiffA = ['pikachu', 'charmander', 'squirtle', 'bulbasaur'];
+var arrayDiffB = ['jigglypuff', 'pikachu'];
+
+var arrayDiffOriginal = "[" + arrayA + "], [" + arrayB + "]";
+var arrayDiffOutput = difference(arrayA, arrayB);
+
+$('.arrayDiffOriginal').text(arrayDiffOriginal);
+$('.arrayDiffOutput').text( JSON.stringify(arrayDiffOutput) );
+
+
+
+
+// -- Array Intersection
+const intersection = (a, b) => {
+	const s = new Set(b);
+	return a.filter(x => s.has(x));
+};
+
+var arrayIntA;
+var arrayIntB;
+
+var arrayIntOriginal;
+var arrayIntOutput;
+
+
+
+
+
+
+
+
