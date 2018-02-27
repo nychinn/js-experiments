f2bcb714df2f1d8d9135e07b5f494b3a7829f17f
Nicole C <nychinn@gmail.com>
2018-02-27 18:41:57 +1300

    continue simon game

--- projects/simon/js/javascript.js
+++ projects/simon/js/javascript.js
@@ -13,6 +13,45 @@
 // check the number pressed, and see if it matches the sequence
 // if it matches, continue. if not, you lose
 
+const colours = [{
+		'num': 1,
+		'colour': 'green'
+	},
+	{
+		'num': 2,
+		'colour': 'red'
+	},
+	{
+		'num': 3,
+		'colour': 'blue'
+	},
+	{
+		'num': 4,
+		'colour': 'yellow'
+	}
+];
+
+// set variable for the sequence computer plays
+let sequence = [];
+
+// set variable for the sequence user plays (clears every round)
+let userSequence = [];
+
+// generate random number from 1 - 4
+const generateNum = function() {
+	return Math.floor(Math.random() * 4) + 1;
+}
+
+const newRound = function() {
+	
+}
+
+// main function that runs the game
+const initGame = function() {
+	sequence.push(generateNum());
+	console.log(sequence);
+}
+
 $('.simonQuarter').on('mousedown', function() {
 	$(this).addClass('j-active');
 	console.log('down');
@@ -22,3 +61,11 @@ $('.simonQuarter').on('mouseup', function() {
 	$(this).removeClass('j-active');
 	console.log('up');
 });
+
+// console.log(generateNum());
+
+$(document).on('click', '.simonStart', initGame);
+
+
+
+
