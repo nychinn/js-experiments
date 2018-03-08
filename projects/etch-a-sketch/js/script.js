648f62671271ffd15e7b766ffca652692fa0b7d6
Nicole C <nychinn@gmail.com>
2018-03-08 17:05:38 +1300

    create input field to edit size

--- projects/etch-a-sketch/js/script.js
+++ projects/etch-a-sketch/js/script.js
@@ -1,40 +1,76 @@
 
-let size = 16;
+let size = 16; 
+let grid = document.querySelector('.etchGrid');
 
 function createGridItems() {
-	var html = '';
-	for ( let i=0; i<(size*size); i++ ) {
+	let html = '';
+	let num = size*size;
+	for ( let i=0; i<num; i++ ) {
 		html += '<div class="block"></div>';
 	}
 	return html;
 }
 
+function newSize() {
+	size = $('.etchSize input').val();
+}
+
 function createGrid() {
-	$('.etchGrid').append(createGridItems());
+	grid.innerHTML = createGridItems();
 
 	$('.block').css('width', (100 / size) + '%' );
 	$('.block').css('padding-bottom', (100 / size) + '%' );
+
+	// let block = document.querySelectorAll('.block');
+
+	// for ( let i=0; i<block.length; i++ ) {
+	// 	console.log(block[i]);
+	// 	block[i].style.width = (100 / size) + '%';
+	// 	block[i].style.paddingBottom = (100 / size) + '%';
+	// }
+
 }
 
 function restartGrid() {
-	size = prompt("What size grid would you like? (eg. 64)");
-	$('.etchGrid').empty();
+	// size = prompt("What size grid would you like? (eg. 64)");
+	grid.innerHTML = '';
 	createGrid();
 }
 
 function clearGrid() {
-	$('.etchGrid').empty();
+	grid.innerHTML = '';
 	createGrid();
 }
 
-$(document).on('mouseenter', '.block', function() {
-	let opacity = $(this).css('opacity');
-	if ( $(this).css('opacity') < 1 ) {
-		$(this).css('opacity', (parseFloat(opacity) + 0.1));
+$(document).on('mouseenter', '.block', function(e) {
+	let opacity = Array(e.target)[0].style.opacity;
+
+	// console.log(Array(e.target)[0].style.opacity);
+
+	if ( opacity < 1 ) {
+		Array(e.target)[0].style.opacity = (parseFloat(opacity) + 0.1)
+	}
+
+	if ( opacity == '' ) {
+		Array(e.target)[0].style.opacity = 0.1;
+	} else if ( opacity < 1 ) {
+		Array(e.target)[0].style.opacity = (parseFloat(opacity) + 0.1);
 	}
 });
 
-$(document).on('click', '.etchRestart', restartGrid);
-$(document).on('click', '.etchClear', clearGrid);
+$(document).on('keyup', function(e) {
+	if ( $('.etchSize input').is(':focus') && e.which == 13) {
+		createGrid();
+	}
+});
+
+$('.etchSize input').on('keyup', function() {
+	newSize();
+});
+
+document.querySelector('.etchRestart').addEventListener('click', restartGrid);
 
 createGrid();
+
+
+
